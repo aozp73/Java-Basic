f4dd3f4ec752822fc75d7cacd24d96024e0fd88d
aozp73 <aozp73@naver.com>
2024-02-24 15:21:31 +0900

    객체 지향 프로그래밍

---
+++ src/poly/car1/CarMain1.java
@@ -0,0 +1,26 @@
+package poly.car1;
+
+/**
+ * 다형성을 활용한 런타임 변경
+ * 런타임: 애플리케이션 실행 도중에 변경 가능
+ */
+public class CarMain1 {
+    public static void main(String[] args) {
+        Driver driver = new Driver();
+
+        // 1. 차량 선택 (k3)
+        Car k3Car = new K3Car();
+        driver.setCar(k3Car);
+        driver.drive();
+
+        // 2. 차량 변경 (k3 -> model3)
+        Car model3Car = new Model3Car();
+        driver.setCar(model3Car);
+        driver.drive();
+
+        // OCP: 새 차량 추가 (model3 -> NewCar)
+        Car newCar = new NewCar();
+        driver.setCar(newCar);
+        driver.drive();
+    }
+}
